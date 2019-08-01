f7105f9510a7a2bdde6c314e9dfc6083428cf0f1
Tarun Kethwalia <tarunkethwlia18@gmail.com>
2019-08-01 17:36:04 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BuyLabour | Home</title>
+  </head>
+  <body>
+    Work In Progress
+  </body>
+</html>
